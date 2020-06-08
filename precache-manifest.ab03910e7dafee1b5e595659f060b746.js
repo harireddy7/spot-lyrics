3996c95bb987ff77a41a6e399cd1403b0117a46b
HariKotha <harimsd8@gmail.com>
2020-06-08 21:02:43 +0530

    Updates

---
+++ precache-manifest.ab03910e7dafee1b5e595659f060b746.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72cf441e3ba1bb8ef7f9a7df03b7c85b",
+    "url": "/spot-lyrics/index.html"
+  },
+  {
+    "revision": "9b3568f130003c678496",
+    "url": "/spot-lyrics/static/css/main.c5ac3eec.chunk.css"
+  },
+  {
+    "revision": "a29143aac382212a8da6",
+    "url": "/spot-lyrics/static/js/2.360ad986.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spot-lyrics/static/js/2.360ad986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b3568f130003c678496",
+    "url": "/spot-lyrics/static/js/main.498c9b48.chunk.js"
+  },
+  {
+    "revision": "f686cc92c0eca1b913d2",
+    "url": "/spot-lyrics/static/js/runtime-main.0e2c2b1b.js"
+  }
+]);
